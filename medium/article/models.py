d58e554e6794f9f5d96d111878e4d9be24638912
esir <esirkings@gmail.com>
2018-10-22 17:08:57 +0300

    Adds a __str__ method to get a readable object representation

--- medium/article/models.py
+++ medium/article/models.py
@@ -6,7 +6,13 @@ class Article(models.Model):
     body = models.TextField()
     author = models.ForeignKey('Author', related_name='articles', on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.title
+
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.name
