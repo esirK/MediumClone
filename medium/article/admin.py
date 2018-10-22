da9ad8e8e5aa285b0e21a89da9651a4814fd6111
esir <esirkings@gmail.com>
2018-10-22 16:06:19 +0300

    Adds Article and Author model

--- medium/article/admin.py
+++ medium/article/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, Author
+
+admin.site.register(Author)
+admin.site.register(Article)
--- medium/article/models.py
+++ medium/article/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    body = models.TextField()
+    author = models.ForeignKey('Author', related_name='articles', on_delete=models.PROTECT)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
