769a78108e13f1e828563e5940b33b0b283ccb37
esir <esirkings@gmail.com>
2018-10-22 16:08:08 +0300

    Adds an api endpoint for getting all articles

---
+++ medium/article/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import ArticleView
+
+
+app_name = "articles"
+
+
+urlpatterns = [
+    path('articles/', ArticleView.as_view()),
+]
--- medium/article/views.py
+++ medium/article/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Article
+
+
+class ArticleView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        return Response({"articles": articles})
