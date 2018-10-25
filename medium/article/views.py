3c143344b4be54f82d14aa05c39dd583d041503d
esir <esirkings@gmail.com>
2018-10-25 11:22:18 +0300

    Allows a user to view the details of a single Article using the article id

--- medium/article/views.py
+++ medium/article/views.py
@@ -6,7 +6,11 @@ from .models import Article
 from .serializers import ArticleSerializer
 
 class ArticleView(APIView):
-    def get(self, request):
+    def get(self, request, pk=None):
+        if pk:
+            article = get_object_or_404(Article.objects.all(), pk=pk)
+            serializer = ArticleSerializer(article)
+            return Response({"article": serializer.data})
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response({"articles": serializer.data})
@@ -21,10 +25,10 @@ class ArticleView(APIView):
         return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
 
     def put(self, request, pk):
-        saved_article = get_object_or_404(Article, pk)
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
         data = request.data.get('article')
-        serializer = ArticleSerializer(instance=saved_article, data=data)
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+
         if serializer.is_valid(raise_exception=True):
             article_saved = serializer.save()
         return Response({"success": "Article '{}' updated successfully".format(article_saved.title)})
-
