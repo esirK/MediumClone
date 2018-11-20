d4d8df51015bb926c494c5278d756ef894d7fadc
esir <esirkings@gmail.com>
2018-11-20 14:51:47 +0300

    Allows a user to create an article and list all articles using Mixins

--- medium/article/views.py
+++ medium/article/views.py
@@ -1,8 +1,20 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import GenericAPIView, get_object_or_404
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 
-from .models import Article
+from .models import Article, Author
 from .serializers import ArticleSerializer
 
-class ArticleView(ListAPIView):
+class ArticleView(ListModelMixin, CreateModelMixin, GenericAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+    def perform_create(self, serializer):
+        author = get_object_or_404(Author, id=self.request.data.get('author_id'))
+        return serializer.save(author=author)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, *kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
