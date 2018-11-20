0f8b1d07bd53832655ef1f38f62a0c18f550b27b
esir <esirkings@gmail.com>
2018-11-20 04:57:31 +0300

    Use a generic ListView instead of basic APIView

--- medium/article/views.py
+++ medium/article/views.py
@@ -1,41 +1,8 @@
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 
 from .models import Article
 from .serializers import ArticleSerializer
 
-class ArticleView(APIView):
-    def get(self, request, pk=None):
-        if pk:
-            article = get_object_or_404(Article.objects.all(), pk=pk)
-            serializer = ArticleSerializer(article)
-            return Response({"article": serializer.data})
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response({"articles": serializer.data})
-
-    def post(self, request):
-        article = request.data.get('article')
-
-        # Create an article from the above data
-        serializer = ArticleSerializer(data=article)
-        if serializer.is_valid(raise_exception=True):
-            article_saved = serializer.save()
-        return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
-
-    def put(self, request, pk):
-        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
-        data = request.data.get('article')
-        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
-
-        if serializer.is_valid(raise_exception=True):
-            article_saved = serializer.save()
-        return Response({"success": "Article '{}' updated successfully".format(article_saved.title)})
-
-
-    def delete(self, request, pk):
-        # Get object with this pk
-        article = get_object_or_404(Article.objects.all(), pk=pk)
-        article.delete()
-        return Response({"message": "Article with id `{}` has been deleted.".format(pk)},status=204)
+class ArticleView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
