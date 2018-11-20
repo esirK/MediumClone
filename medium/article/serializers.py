86cf83704da4802cff53e5337d7a16bf9340598f
esir <esirkings@gmail.com>
2018-11-20 04:58:03 +0300

    Use a ModelSerializer instead of a basic Serializer

--- medium/article/serializers.py
+++ medium/article/serializers.py
@@ -3,20 +3,7 @@ from rest_framework import serializers
 from .models import Article
 
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=120)
-    description = serializers.CharField()
-    body = serializers.CharField()
-    author_id = serializers.IntegerField()
-
-    def create(self, validated_data):
-        return Article.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.body = validated_data.get('body', instance.body)
-        instance.author_id = validated_data.get('author_id', instance.author_id)
-
-        instance.save()
-        return instance
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ('id', 'title', 'description', 'body', 'author_id')
