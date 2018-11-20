d81ca305386a41effa56c1b1a2e7d4587247f978
esir <esirkings@gmail.com>
2018-11-20 15:01:25 +0300

    Uses generics instead of mixins

--- medium/article/views.py
+++ medium/article/views.py
@@ -1,20 +1,12 @@
-from rest_framework.generics import GenericAPIView, get_object_or_404
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.generics import CreateAPIView, ListAPIView, get_object_or_404
 
 from .models import Article, Author
 from .serializers import ArticleSerializer
 
-class ArticleView(ListModelMixin, CreateModelMixin, GenericAPIView):
+class ArticleView(CreateAPIView, ListAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
     def perform_create(self, serializer):
         author = get_object_or_404(Author, id=self.request.data.get('author_id'))
         return serializer.save(author=author)
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, *kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
